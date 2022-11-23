0e0a913c7b4d530045de82dbbd408018955ee570
Alessandro Amella <info@bitrey.it>
2022-11-23 23:24:31 +0100

    add .gitignore and remove useless files from future commits

--- CMakeFiles/TheSnowCosimo.dir/backend/components/map/Level.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/TheSnowCosimo.dir/backend/components/map/Level.cpp.o: \
- /home/bitrey/TheSnowCosimo/backend/components/map/Level.cpp \
- /usr/include/stdc-predef.h \
- /home/bitrey/TheSnowCosimo/backend/components/map/Level.h \
- /home/bitrey/TheSnowCosimo/backend/components/map/chunk/Chunk.h
--- CMakeFiles/TheSnowCosimo.dir/backend/components/map/chunk/Chunk.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/TheSnowCosimo.dir/backend/components/map/chunk/Chunk.cpp.o: \
- /home/bitrey/TheSnowCosimo/backend/components/map/chunk/Chunk.cpp \
- /usr/include/stdc-predef.h \
- /home/bitrey/TheSnowCosimo/backend/components/map/chunk/Chunk.h
