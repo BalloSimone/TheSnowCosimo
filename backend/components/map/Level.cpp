4fe51e5334b33732f8397952e5764be8fefe111c
simo <simoneballo84@gmail.com>
2022-11-28 10:41:51 +0100

    start project

--- backend/components/map/Level.cpp
+++ backend/components/map/Level.cpp
@@ -7,7 +7,7 @@ Level::Level() {
 
 void Level::generateLevel() {
     int app = -1;
-    for(int i=0; i<N_CHUNCKS; i++){
+    for(int i=0; i<N_CHUNKS; i++){
         chunks[i].create_chunk(app);
         app = chunks[i].floor_y[CHUNK_DIMENSION-1];
     }
